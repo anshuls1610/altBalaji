022c0dd41c4c4e14260e0a3e801b893ad79927a4
Anshul1610 <anshul.s@frendy.in>
2022-02-17 20:36:38 +0530

    mock login in controller

--- app.js
+++ app.js
@@ -2,9 +2,7 @@ const express = require('express');
 const app = express();
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcryptjs');
-const config = require('./config/config');
+const loginController = require('./controllers/login');
 
 app.use(express.json({ limit: '5mb' }));
 app.use(express.urlencoded({
@@ -17,29 +15,8 @@ app.use(cookieParser());
 const indexRouter = require('./routes/index');
 const userRouter = require('./routes/user');
 
-app.all('*', (req, res, next) => {
-    let user = {
-        id: 1,
-        name: 'admin',
-        password: bcrypt.hashSync('admin', 8)
-    }
-
-    var passwordIsValid = bcrypt.compareSync(
-        'admin',
-        user.password
-    );
-
-    if (!passwordIsValid) {
-        return res.status(401).send({
-            accessToken: null,
-            message: "Invalid Password!"
-        });
-    }
-
-    var token = jwt.sign({ id: user.id }, config.secret, {
-        expiresIn: 86400 // 24 hours
-    });
-    req.headers['x-access-token'] = token;
+app.all('*', async (req, res, next) => {
+    await loginController(req);
     next();
 });
 
