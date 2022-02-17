d381e1c2e5b4087b53f7a7ba41cad1f147b8e449
Anshul1610 <anshul.s@frendy.in>
2022-02-17 20:02:51 +0530

    jwt middleware added

---
+++ middleware/authJwt.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken');
+const config = require('../config/config');
+
+module.exports = (req, res, next) => {
+    const token = req.body.token || req.query.token || req.headers['x-access-token'];
+    console.log('here in jwt token is: ', token);
+
+    if (!token) {
+        return res.status(403).send({
+            message: 'No token provided!'
+        });
+    }
+
+    jwt.verify(token, config.secret, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({
+                message: 'Unauthorized!'
+            });
+        }
+        req.userId = decoded.id;
+        console.log('here in jwt verifying user: ', req.userId);
+        next();
+    });
+};
